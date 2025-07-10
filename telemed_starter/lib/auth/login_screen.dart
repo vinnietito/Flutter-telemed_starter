5446de747c5bd15ff3125bad5713c85d984c2705
vinnietito <vincentnjuguna69@gmail.com>
2025-07-10 23:37:34 +0300

    Commits

--- telemed_starter/lib/auth/login_screen.dart
+++ telemed_starter/lib/auth/login_screen.dart
@@ -5,11 +5,11 @@ import 'register_screen.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginScreen extends StatefulWidget {
-  @overide
+  @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState  extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
@@ -26,5 +26,34 @@ class _LoginScreenState  extends State<LoginScreen> {
     } catch (e) {
       Fluttertoast.showToast(msg: "Login failed: $e");
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('TeleMed Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(children: [
+          TextField(
+            controller: emailController,
+            decoration: InputDecoration(labelText: 'Email'),
+          ),
+          TextField(
+            controller: passwordController,
+            obscureText: true,
+            decoration: InputDecoration(labelText: 'Password'),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(onPressed: login, child: Text('Login')),
+          TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => RegisterScreen()));
+              },
+              child: Text("Don't have an account? Register"))
+        ]),
+      ),
+    );
   }
 }
