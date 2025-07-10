3bec7a4e31c813f50820aa7687905da7823d8089
vinnietito <vincentnjuguna69@gmail.com>
2025-07-10 23:36:55 +0300

    Commits

--- telemed_starter/lib/auth/login_screen.dart
+++ telemed_starter/lib/auth/login_screen.dart
@@ -3,3 +3,28 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:telemed_starter/screens/home_screen.dart';
 import 'register_screen.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
+class LoginScreen extends StatefulWidget {
+  @overide
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState  extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void login() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => HomeScreen()),
+      );
+    } catch (e) {
+      Fluttertoast.showToast(msg: "Login failed: $e");
+    }
+  }
+}
