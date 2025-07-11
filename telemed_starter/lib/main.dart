205805bea1b32a36287fb82efb6e264fa5a1d0df
vinnietito <vincentnjuguna69@gmail.com>
2025-07-11 10:56:48 +0300

    Commits

--- telemed_starter/lib/main.dart
+++ telemed_starter/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'auth/login_screen.dart';
-import 'firebase_options.dart'; // ✅ required
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform, // ✅ important for web/desktop
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
 }
